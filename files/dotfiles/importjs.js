37f9dcd4a18a11838782e70490678365d3c91b6c
Diego Romero <jd@inkatze.com>
2020-07-22 22:04:15 -0700

    Adds more importjs configuration options

--- files/dotfiles/importjs.js
+++ files/dotfiles/importjs.js
@@ -2,15 +2,20 @@ module.exports = {
   aliases: {
     classes: "./{filename}.scss",
   },
-  environments: ["browser", "jest"],
+  environments: ["es2017", "browser", "jest", "builtin"],
   sortImports: false,
   useRelativePaths: false,
   excludes: ["./public/assets/packs/js/**"],
+  maxLineLength: 120,
   moduleNameFormatter({ moduleName, pathToImportedModule }) {
     if (!pathToImportedModule) return moduleName;
     if (pathToImportedModule.includes("spec"))
       return pathToImportedModule
         .replace(/.*spec\//, "spec/")
+        .replace(/(\.js.*|\.ts.*)/, "");
+    if (pathToImportedModule.includes("test-utils"))
+      return pathToImportedModule
+        .replace(/.*test-utils\//, "test-utils/")
         .replace(/(\.js.*|\.ts.*)/, "");
     if (pathToImportedModule.includes("accountants"))
       return pathToImportedModule
