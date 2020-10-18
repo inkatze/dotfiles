b55ba94d9146bf47b66d990f9c4896770764ac11
Diego Romero <jd@inkatze.com>
2020-10-18 13:34:17 -0700

    Updates install command

--- files/fish/conf.d/gusto_funcs.fish
+++ files/fish/conf.d/gusto_funcs.fish
@@ -20,7 +20,7 @@ end
 function zeni
   bundle install; and \
     yarn install; and \
-    brails db:migrate; and \
+    brails db:create db:migrate db:test:prepare; and \
     zentags; and \
     solarup
 end
