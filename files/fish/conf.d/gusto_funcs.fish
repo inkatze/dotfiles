2a21db204223716fd4daf7c792195633b3ed9b9b
Diego Romero <jd@inkatze.com>
2021-08-16 12:53:21 -0700

    Removes ctags generation

--- files/fish/conf.d/gusto_funcs.fish
+++ files/fish/conf.d/gusto_funcs.fish
@@ -3,25 +3,10 @@ function gustograph
     npx yarn generate-client-types
 end
 
-function zentags
-  # --output-format=json is better, but it's not suppoorted by FZF
-  ctags -R \
-    --languages=all \
-    --exclude=.git \
-    --exclude=node_modules \
-    --exclude=frontend/javascripts \
-    --exclude=config \
-    --exclude=script \
-    --exclude=public \
-    --verbose=no \
-    -f tags
-end
-
 function zeni
   bundle install; and \
     yarn install; and \
-    brails db:create db:migrate db:test:prepare; and \
-    zentags
+    brails db:create db:migrate db:test:prepare
 end
 
 function zensync
