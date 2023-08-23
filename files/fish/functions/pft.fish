89fa2705d1dba7ceed473671352dcd0ec510728a
Diego Romero <jd@inkatze.com>
2023-08-23 14:49:20 -0700

    Updates custom test function

--- files/fish/functions/pft.fish
+++ files/fish/functions/pft.fish
@@ -1,4 +1,9 @@
-function pftest -w rspec -a test -a overwrite -a state
+function pft -w rspec -a test -a state -a overwrite
+    if not test -n "$test"
+        echo 'usage: pft [test_path] [state] [overwrite]'
+        return 1
+    end
+
     set -xl TEST_COMMAND (if test -e ./bin/rspec; echo ./bin/rspec; else; echo "bundle exec rspec"; end)
     set -xl TEST_FILE (if test -n "$test"; echo $test; else; echo .; end)
     set -xl OVERWRITE_FIXTURES (if test -n "$overwrite"; echo "$overwrite"; else; echo 1; end)
