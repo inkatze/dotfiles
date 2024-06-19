d9e2118aa2a6668ad12b898e571c428181cad3ec
Diego Romero <jd@inkatze.com>
2024-06-19 10:56:25 -0700

    Updates jdtls launch command

--- roles/neovim/files/nvim/ftplugin/java.lua
+++ roles/neovim/files/nvim/ftplugin/java.lua
@@ -20,7 +20,14 @@ capabilities = {
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local config = {
-  cmd = {jdtls_dir .. "/bin/jdtls"},
+  cmd = {
+    jdtls_dir .. "/bin/jdtls",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens", "java.base/java.util=ALL-UNNAMED",
+    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+    "-configuration", jdtls_dir .. "/config_mac_arm",
+    "-data", os.getenv("HOME") .. "/.cache/jdtls/workspace"
+  },
   root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
   capabilities = capabilities,
   on_attach = require("inkatze.lspconfig").on_attach,
