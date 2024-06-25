85e3e16573e1bee360a5ed573b9591cf6e5d1d7f
Diego Romero <jd@inkatze.com>
2024-06-25 14:09:18 -0700

    Updates ftplugins

--- roles/neovim/files/nvim/ftplugin/groovy.lua
+++ roles/neovim/files/nvim/ftplugin/groovy.lua
@@ -1,4 +1,5 @@
-vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
+vim.opt_local.autoindent = true
+vim.opt_local.expandtab = false
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
