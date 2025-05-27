c9a5d9598f0d79d6c848e29b3acde31b92c62a5e
Diego Romero <jd@inkatze.com>
2025-05-27 14:43:21 -0700

    Fixes zenpull command

--- roles/fish/files/fish/functions/zenpull.fish
+++ roles/fish/files/fish/functions/zenpull.fish
@@ -1,16 +1,14 @@
-function usage
-  echo 'usage: zenpull [bash_command] [notification]'
-  return 1
-end
-
 function zenpull
-  if test -z "$argv[1]"
-    or test -z "$argv[2]"
-    return usage()
-  end
+    set -xl default_notification 'company data pull completed'
 
-  set -xl bash_command $argv[1]
-  set -xl notification $argv[2]
+    if test -z "$argv"
+        echo "usage: zenpull [bash_command] [notification | '$default_notification']"
+        return 1
+    end
+
+    set -xl bash_command $argv[1]
+    set -xl notification $default_notification
+    test -n "$argv[2]"; and set -xl notification $argv[2]
 
   bash -c $bash_command;
     and bin/rails db:migrate;
