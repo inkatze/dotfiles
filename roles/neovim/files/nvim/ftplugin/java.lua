e134d7663ff6f113e60f3c3e5475def90805ef61
Diego Romero <jd@inkatze.com>
2025-06-03 15:26:41 -0700

    Update java's vim configuration

--- roles/neovim/files/nvim/ftplugin/java.lua
+++ roles/neovim/files/nvim/ftplugin/java.lua
@@ -6,10 +6,20 @@ vim.opt_local.softtabstop = 2
 
 local base_dir = os.getenv("HOME") .. "/dev"
 local jdtls_dir = base_dir .. "/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository"
-local bundles = {
-  vim.fn.glob(base_dir .. "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", true),
-};
-vim.list_extend(bundles, vim.split(vim.fn.glob(base_dir .. "/vscode-java-test/server/*.jar", true), "\n"))
+
+local bundles = {}
+for _, jar in ipairs(vim.split(vim.fn.glob(
+  base_dir .. "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+  true
+), "\n")) do
+  if #jar > 0 then
+    table.insert(bundles, jar)
+  end
+end
+-- vim.list_extend(bundles, vim.split(vim.fn.glob(base_dir .. "/vscode-java-test/server/*.jar", true), "\n"))
+
+local project_root = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1])
+local workspace = os.getenv("HOME") .. "/.cache/jdtls/workspace"
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities = {
@@ -26,18 +36,41 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local config = {
   cmd = {
-    jdtls_dir .. "/bin/jdtls",
+    "mise", "exec", "--", "java",
+    "--enable-preview",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dpolyglot.ruby.options=--dap --dap-host=0.0.0.0 --dap-port=5005",
+    "-Dlog.level=ALL",
+    "-Xmx2G",
     "--add-modules=ALL-SYSTEM",
     "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+    "-jar", jdtls_dir .. "/plugins/org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar",
     "-configuration", jdtls_dir .. "/config_mac_arm",
-    "-data", os.getenv("HOME") .. "/.cache/jdtls/workspace"
+    "-data", workspace
   },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  root_dir = project_root,
   capabilities = capabilities,
   on_attach = require("inkatze.lspconfig").on_attach,
   init_options = {
     bundles = bundles,
+  },
+  settings = {
+    java = {
+      import = {
+        gradle = {
+          enabled = true,
+          wrapper = {
+            enabled = true,
+          },
+          annotationProcessing = {
+            enabled = true,
+          },
+        },
+      },
+    }
   }
 }
 
