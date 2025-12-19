983b9eda473cf0ef533ef3b3cd9feae77592cf0b
Diego Romero <jd@inkatze.com>
2025-12-19 15:05:03 -0800

    Use tnotify wrapper for notifications

---
+++ roles/fish/files/fish/functions/tnotify.fish
@@ -0,0 +1,10 @@
+function tnotify -d "Terminal notification that works in tmux + SSH"
+    if set -q TMUX
+        # Wrap for tmux passthrough: double all ESC chars and wrap in DCS
+        set -l esc (printf '\e')
+        set -l code (kitten notify --only-print-escape-code $argv | sed "s/$esc/$esc$esc/g")
+        printf '\ePtmux;%s\e\\' "$code"
+    else
+        kitten notify $argv
+    end
+end
